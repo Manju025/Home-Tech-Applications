4930f7fe48881a80b05bc957cef12f4eeec36d60
Manjundhar <103714273+Manju025@users.noreply.github.com>
2025-08-01 00:37:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -570,7 +570,7 @@ function setupFormEvents() {
     if (orderForm) {
         orderForm.addEventListener('submit', function(e) {
             e.preventDefault();
-            processOrder();
+            processOrder(new FormData(this));
         });
     }
 }
